788de2ef612a54d464d18e82eefcb753319e5fc9
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-12-18 22:55:09 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -5,6 +5,7 @@ import time
 import spamwatch
 
 import telegram.ext as tg
+from redis import StrictRedis
 from telethon import TelegramClient
 
 StartTime = time.time()
@@ -137,6 +138,7 @@ else:
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
+    REDIS_URL = os.environ.get('REDIS_URL')
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
 
@@ -166,6 +168,25 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
+
+REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
+
+try:
+
+    REDIS.ping()
+
+    LOGGER.info("Your redis server is now alive!")
+
+except BaseException:
+
+    raise Exception("Your redis server is not alive, please check again.")
+
+finally:
+
+   REDIS.ping()
+
+   LOGGER.info("Your redis server is now alive!")
+    
 
 if not SPAMWATCH_API:
     sw = None
